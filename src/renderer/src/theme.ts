998f3367cb0c623d04f170fc9af94ddb269e1726
autroshot <autroshot@gmail.com>
2023-08-16 01:03:45 +0900

    차크라 커스텀 테마 적용하고 테이블에 커스텀 스타일 적용

---
+++ src/renderer/src/theme.ts
@@ -0,0 +1,20 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  components: {
+    Table: {
+      variants: {
+        simple: {
+          th: {
+            borderWidth: '0px 1px 1px 1px',
+          },
+          td: {
+            borderWidth: '1px 1px 0px 1px',
+          },
+        },
+      },
+    },
+  },
+});
+
+export default theme;
